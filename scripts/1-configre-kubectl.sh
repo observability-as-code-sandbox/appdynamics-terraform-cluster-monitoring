b5d4203139a56bb1363d425c186ac0b67e30e07e
alex_jov <aleksjovovic@gmail.com>
2021-08-13 22:55:29 +0100

    Delete manual deployment scripts;

--- scripts/1-configre-kubectl.sh
+++
@@ -1,6 +0,0 @@
-# https://learn.hashicorp.com/tutorials/terraform/eks
-
-# To run the following command, you must have permission to use the eks:DescribeCluster 
-# API action with the cluster that you specify. For more information, see Amazon EKS identity-based policy examples.
-
-aws eks --region $(terraform output -raw region) update-kubeconfig --name $(terraform output -raw cluster_name)
--- scripts/3-example-applications.sh
+++
@@ -1,7 +0,0 @@
-kubectl create ns dev
-
-kubectl create sa appd-account
-
-kubectl apply -f ./application-examples/. -n dev
-
-kubectl delete -f ./application-examples/. -n dev
