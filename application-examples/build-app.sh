93ceab5c195772450a9c9ae5316864d8c66efc02
alex_jov <aleksjovovic@gmail.com>
2022-03-07 11:51:28 +0000

    Add example apps to default namespace;

--- application-examples/build-app.sh
+++ application-examples/build-app.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Create namespace
-kubectl create namespace test
+#kubectl create namespace test
 
 # Deploy the applications
-kubectl -n test apply -f dotnet-deployment.yml
-kubectl -n test apply -f java-deployment.yml
+kubectl -n default apply -f dotnet-deployment.yml
+kubectl -n default apply -f java-deployment.yml
 
 sleep 3
 
 # Verify the deployment
-kubectl -n test get deployments
-kubectl -n test get services
+kubectl -n default get deployments
+kubectl -n default get services
 
 echo "========Now running the below pods in test namespace========"
-kubectl -n test get pods -o wide
+kubectl -n default get pods -o wide
--- application-examples/clean-app.sh
+++ application-examples/clean-app.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # This script undeploys the application the from cluster
 
-kubectl -n test delete -f dotnet-deployment.yml
-kubectl -n test delete -f java-deployment.yml
+kubectl -n default delete -f dotnet-deployment.yml
+kubectl -n default delete -f java-deployment.yml
 
 # Verify the deployment
 echo "========Now running the below pods in test namespace========"
-kubectl -n test get pods -o wide
+kubectl -n default get pods -o wide
 
 # Delete namespace
-kubectl delete namespace test
+#kubectl delete namespace test
