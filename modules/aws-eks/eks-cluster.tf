6df78bfba7cb313c00ec41484b89e3744c4f1901
alex_jov <aleksjovovic@gmail.com>
2022-03-07 11:52:01 +0000

    Specify AWS EKS module version;

--- modules/aws-eks/eks-cluster.tf
+++ modules/aws-eks/eks-cluster.tf
@@ -2,7 +2,8 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = local.cluster_name
   cluster_version = "1.20"
-  subnets         = module.vpc.private_subnets
+  version = "17.24.0"
+  subnets         = module.vpc.private_subnets  
 
   tags = {
     Environment = "training"
